152b29604902cb1d8b7c48c6533a9df85fd00b63
coderclcl <gattenmaster@gmail.com>
2022-05-24 16:42:26 +0900

    fix: DB스키마 변경

--- models/emotion.js
+++ models/emotion.js
@@ -1,26 +1,18 @@
 const Sequelize = require("sequelize");
 
-module.exports = class User extends Sequelize.Model {
+module.exports = class Emotion extends Sequelize.Model {
   static init(sequelize) {
     return super.init(
       {
-        userId: {
+        emotionId: {
           primaryKey: true,
           type: Sequelize.INTEGER.UNSIGNED,
           autoIncrement: true,
           allowNull: false,
           unique: true,
         },
-        email: {
-          type: "varchar(30)",
-          allowNull: true,
-        },
-        password: {
-          type: "varchar(10)",
-          allowNull: false,
-        },
         name: {
-          type: "varchar(6)",
+          type: "varchar(10)",
           allowNull: true,
         },
         createdAt: {
@@ -36,8 +28,8 @@ module.exports = class User extends Sequelize.Model {
       },
       {
         sequelize,
-        modelName: "User",
-        tableName: "User",
+        modelName: "Emotion",
+        tableName: "Emotion",
         charset: "utf8",
         collate: "utf8_general_ci",
         initialAutoIncrement: 1,
@@ -49,21 +41,8 @@ module.exports = class User extends Sequelize.Model {
   }
 
   static associate(db) {
-    db.User.hasMany(db.Pin, {
-      foreignKey: "userId",
-    });
-    db.User.hasMany(db.Group, {
-      foreignKey: "userId",
-    });
-    db.User.belongsToMany(db.User, {
-      foreignKey: "followingId",
-      as: "Followers",
-      through: "Follow",
-    });
-    db.User.belongsToMany(db.User, {
-      foreignKey: "followerId",
-      as: "Followings",
-      through: "Follow",
+    db.Emotion.hasMany(db.Pin, {
+      foreignKey: "emotionId",
     });
   }
 };
--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ const config = require("../config/config")[env];
 const User = require("./user");
 const Category = require("./category");
 const Pin = require("./pin");
+const Group = require("./group");
+const Emotion = require("./emotion");
 // const Follow = require("./follow");
 
 const db = {};
@@ -19,16 +21,22 @@ db.sequelize = sequelize;
 db.User = User;
 db.Category = Category;
 db.Pin = Pin;
+db.Group = Group;
+db.Emotion = Emotion;
 // db.Follow = Follow;
 
 User.init(sequelize);
 Category.init(sequelize);
 Pin.init(sequelize);
+Group.init(sequelize);
+Emotion.init(sequelize);
 // Follow.init(sequelize);
 
 User.associate(db);
 Category.associate(db);
 Pin.associate(db);
+Group.associate(db);
+Emotion.associate(db);
 // Follow.associate(db);
 
 module.exports = db;
