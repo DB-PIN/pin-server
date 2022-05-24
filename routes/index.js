8babb34d23099ddc0100249bdcea51d5ebd87d50
coderclcl <gattenmaster@gmail.com>
2022-05-24 19:45:27 +0900

    feat: 카테고리 api 연결

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.use("/user", require("./user")); // api/user/
 router.use("/pins", require("./pin")); // api/pins/
 router.use("/emotions", require("./emotion")); // api/emotions
+router.use("/categories", require("./category")); // api/categories
 
 module.exports = router;
